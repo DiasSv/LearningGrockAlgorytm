cf9cf17c605169276bc5557fba04992de77f9af1
DiasSv <dima.sveshnikov.81@mail.ru>
2021-06-16 14:53:08 +0300

    Добавлен пример применения бинарного поиска

---
+++ binar_search.py
@@ -0,0 +1,22 @@
+def binary_search(list, item):
+    low = 0
+    high = len(list) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        guess = list[mid]
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+
+my_list = [i for i in range(0,101,4)]
+
+print(binary_search(my_list, 56))
+print(binary_search(my_list, -1))
+
+
